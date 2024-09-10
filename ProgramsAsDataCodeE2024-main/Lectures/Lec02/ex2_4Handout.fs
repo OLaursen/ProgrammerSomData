85c85a72e817c516dc82529709b340bb962bb4da
seve0039 <sevm@itu.dk>
2024-09-10 14:23:20 +0200

    3.2 regex

--- ProgramsAsDataCodeE2024-main/Lectures/Lec02/ex2_4Handout.fs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec02/ex2_4Handout.fs
@@ -4,14 +4,25 @@
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
 let sinstrToInt = function
   | SCstI i -> [0;i]
-  | SVar i  -> failwith "Not implemented"
-  | SAdd    -> failwith "Not implemented"
-  | SSub    -> failwith "Not implemented"
-  | SMul    -> failwith "Not implemented"
-  | SPop    -> failwith "Not implemented"
-  | SSwap   -> failwith "Not implemented"
+  | SVar i  -> [1;i]
+  | SAdd    -> [2]
+  | SSub    -> [3]
+  | SMul    -> [4]
+  | SPop    -> [5]
+  | SSwap   -> [6]
 
-let assemble instrs = failwith "Not implemented"
+let assemble (inss : sinstr list) : int list =
+    let rec assemble' inss acc =
+        match inss with
+        | [] -> List.rev acc
+        | SCstI i :: rest -> assemble' rest (i :: 0 :: acc)
+        | SVar i :: rest -> assemble' rest (i :: 1 :: acc)
+        | SAdd :: rest -> assemble' rest ( 2 :: acc)
+        | SSub :: rest -> assemble' rest (3 :: acc)
+        | SMul :: rest -> assemble' rest (4 :: acc)
+        | SPop :: rest -> assemble' rest (5 :: acc)
+        | SSwap :: rest -> assemble' rest (6 :: acc)
+    assemble' inss []
 
 (* Output the integers in list inss to the text file called fname: *)
 
