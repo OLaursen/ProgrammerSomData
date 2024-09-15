de09ad673ae3ad9a730ad60d5fc085edf981b124
olau <olau@itu.dk>
2024-09-15 21:31:45 +0200

    ds

--- ProgramsAsDataCodeE2024-main/Lectures/Lec03/Assignment3/Expr/obj/Debug/net8.0/Expr.AssemblyInfo.fs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec03/Assignment3/Expr/obj/Debug/net8.0/Expr.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Expr")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06af9b6761495358331b1079b81bf2fb5f54864e")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3a3ac24345047018b4977d5667ed0107b6b08b")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Expr")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Expr")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
