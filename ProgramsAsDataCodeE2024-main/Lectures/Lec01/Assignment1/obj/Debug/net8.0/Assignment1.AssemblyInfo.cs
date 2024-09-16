b0be49b8e1c3cfe2aa6f0679da2325b48cc17cd4
seve0039 <sevm@itu.dk>
2024-09-16 14:47:57 +0200

    Should work now

--- ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06af9b6761495358331b1079b81bf2fb5f54864e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de09ad673ae3ad9a730ad60d5fc085edf981b124")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProgramsAsDataCodeE2024-main/Lectures/Lec03/Assignment3/Expr/ExprLex.fsi
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec03/Assignment3/Expr/ExprLex.fsi
@@ -1,5 +1,5 @@
 
 module ExprLex
-open Microsoft.FSharp.Text.Lexing
+open FSharp.Text.Lexing
 open ExprPar/// Rule Token
 val Token: lexbuf: LexBuffer<char> -> token
