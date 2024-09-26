a47f2568d6ed46ee886136c4e17a722fdb7cd1b0
Davdu <madsd@itu.dk>
2024-09-26 10:24:39 +0200

    Lec05

--- ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7e72728f52a515322903d329d4f27a51fd8ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeba21ecd455dab82b92b07efadd7d852895198f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
