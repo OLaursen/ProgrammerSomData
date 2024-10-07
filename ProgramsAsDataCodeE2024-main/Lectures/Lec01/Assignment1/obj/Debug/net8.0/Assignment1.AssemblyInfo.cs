1f8bd211ef534bfc9b496c767fccdcfe39d05a17
seve0039 <sevm@itu.dk>
2024-10-07 20:00:09 +0200

    Transitive and a -> b

--- ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8054a5d202741bc5b280737a297abe02bbf4db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f634fcbee0679cac674e67895ee8609960f1a691")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProgramsAsDataCodeE2024-main/Lectures/Lec05/Solution.md
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec05/Solution.md
@@ -106,10 +106,18 @@ let f x = let g y = let h z = z in h x end in g end in f end
 
 let f x = let g y = y in g end in f end
 
+         f            g             h
 ### ('a -> 'b) -> ('b -> c) -> ('a -> 'c)
 
-### 'a -> 'b
+let f x =
+    let h z =
+        let g y = 
+            x (z y)
+        in g end
+    in h end 
+in f end
 
-let f x = x in f end
+### 'a -> 'b
+let f x = f x in f end
 
 ### 'a
