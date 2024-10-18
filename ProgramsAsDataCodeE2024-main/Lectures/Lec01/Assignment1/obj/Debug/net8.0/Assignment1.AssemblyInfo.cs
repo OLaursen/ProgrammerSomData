c72a051cd27af399ddadc5c6baaa44ff428b7b3e
seve0039 <sevm@itu.dk>
2024-10-18 17:09:09 +0200

    Added Lec6 code from repo

--- ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec01/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+019db380fe6c74c8aa2013fe0e4101ae6c50663e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad81cb185acd7cbfb8b930fc3407e21a0d8de36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
