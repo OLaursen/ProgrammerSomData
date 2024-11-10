8ebffafc58d1f15012b4105a75861b44fe05ed02
Mads Dalgaard Andersen <madsd@itu.dk>
2024-11-10 22:04:06 +0100

    Hello world'n shit

---
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec09/Handin8.md
@@ -0,0 +1,22 @@
+# Handin 8
+
+
+
+## Exercise 9.1
+
+see virtual/Selsort/Selsort.il 
+and virtual/Selsort/Selsort.jvmbytecode
+
+
+## Exercise 9.2
+see virtual/StringConcatSpeed/KvantitativDatamålingStringConcat.png 
+and virtual/StringConcatSpeed/KvantitativDatamålingOtherApp.png
+
+(We produced some code to track with perfmon. See TestProj)
+
+
+
+## Exercise 9.3 (We chose to do 9.2)
+
+
+
---
+++ ProgramsAsDataCodeE2024-main/Lectures/Lec09/TestProj/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Text;		// For StringBuilder
+using System.Diagnostics;       // For Stopwatch
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(String[] args)
+	{
+		Queue<Queue<int>> q = new();
+
+		Console.WriteLine("Press return to continue...");
+		Console.ReadLine();
+
+		buildList(q, 50000000);
+		Console.WriteLine("Initialization: Building array of small strings");
+
+		destroyList(q, 50000000);
+
+		buildList(q, 50000000);
+
+		destroyList(q, 50000000);
+
+		Console.WriteLine("Finished!");
+	}
+
+	private static void buildList(Queue<Queue<int>> q, int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			q.Enqueue(new Queue<int>());
+		}
+	}
+
+	private static void destroyList(Queue<Queue<int>> q, int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			q.Dequeue();
+		}
+	}
+}
+
